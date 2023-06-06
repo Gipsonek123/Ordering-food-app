107b70eb33c4c706ff880b6b2c580de2784fa8c2
TymotP <tymekxon@gmail.com>
2023-06-06 21:39:13 +0200

    Adding a database connection

--- ordering-food-app-2/main.cpp
+++ ordering-food-app-2/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    dbManager db_connected = dbManager("C:/programowanie/projekt/ordering-food-app-2/database/foodDatabase.db");
+    dbManager db_connected = dbManager("../database/foodDatabase.db");
     db_connected.writeFood();
     w.show();
     return a.exec();
